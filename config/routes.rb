72b294a748f9d18375b0eccbd69d7153ed74e1d5
Chandan Kumar <chandankumar.r@gmail.com>
2015-04-11 02:37:40 +0530

    Update Application layout to use Bootstrap CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
